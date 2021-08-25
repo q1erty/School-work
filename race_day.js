1d4934d8d9486714069827e48e7e234d69b2cb0d
q1erty <beleckaaolga30@gmail.com>
2021-08-25 11:34:27 +0300

    Update race_day.js

--- race_day.js
+++ race_day.js
@@ -1,1 +1,20 @@
-
+let raceNumber = Math.floor(Math.random() * 1000;
+let early = false;
+let age = 18;
+                            
+if (early && age > 18) {                            
+  raceNumber += 1000;                          
+}
+if (early && age > 18) {
+  console.log(`Race will begin at 9:30, your race number is: ${raceNumber}.`);
+  }
+else if (!early && age > 18) {
+  console.log(`Race will begin at 11:00, your race number is: ${raceNumber}.`);
+}
+else if {age < 18) {
+  console.log(`Race will begin at 12:30, your race number is: ${raceNumber}.`);
+}
+         else {
+           console.log("See the registration desk.");
+         }
+  
