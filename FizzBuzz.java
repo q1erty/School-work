80ccd694bff1b0d4ce3a86d7a3ca0e681d910bd6
q1erty <beleckaaolga30@gmail.com>
2021-08-30 08:24:58 +0300

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,7 @@
 class FizzBuzz {
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++){
-      if ((i % 3 == 0) && (i & 5 == 0)){
+      if ((i % 3 == 0) && (i % 5 == 0)){
         System.out.println("FizzBuzz");
       }
       else if (i % 3 == 0){
